5a64df84e080c67b8532cf43430acecd78076134
Komlyk Dmytro <dima.komlyk@icloud.com>
2020-05-01 11:37:59 +0200

    Step 2 - modify the index.js file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const data = () => {
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .parse(process.argv);
+    .option('-f, --format [type]', 'output format')
+    .arguments('<firstConfig> <secondConfig>');
+
+  program.parse(process.argv);
 };
+
 
 export default data;
